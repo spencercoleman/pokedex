723477f6e4923a03399199ad7d0baf32a3158001
spencercoleman <spencer@edc.vc>
2023-01-19 00:20:13 -0800

    Resolve index error for pokemon types

--- utils/pokemonHelpers.ts
+++ utils/pokemonHelpers.ts
@@ -1,29 +1,29 @@
-export const typeColors = {
-	bug: '#99C32E',
-	dark: '#5F5A6B',
-	dragon: '#086CC0',
-	electric: '#F4D644',
-	fairy: '#EB92E5',
-	fighting: '#D34364',
-	fire: '#FEA152',
-	flying: '#93ADDF',
-	ghost: '#5F6CBA',
-	grass: '#5CBB57',
-	ground: '#D97F4D',
-	ice: '#79D1C6',
-	normal: '#919B9F',
-	poison: '#A965C7',
-	psychic: '#F77277',
-	rock: '#C8B88B',
-	steel: '#548D9E',
-	water: '#59A0DB',
+export const typeColors: { [key: string]: string } = {
+    bug: '#99C32E',
+    dark: '#5F5A6B',
+    dragon: '#086CC0',
+    electric: '#F4D644',
+    fairy: '#EB92E5',
+    fighting: '#D34364',
+    fire: '#FEA152',
+    flying: '#93ADDF',
+    ghost: '#5F6CBA',
+    grass: '#5CBB57',
+    ground: '#D97F4D',
+    ice: '#79D1C6',
+    normal: '#919B9F',
+    poison: '#A965C7',
+    psychic: '#F77277',
+    rock: '#C8B88B',
+    steel: '#548D9E',
+    water: '#59A0DB',
 };
 
 export const formatPokemonID = (id: number) => {
-	const stringId = `${id}`;
-	return stringId.padStart(3, '0');
+    const stringId = `${id}`;
+    return stringId.padStart(3, '0');
 };
 
 export const formatText = (text: string) => {
-	return text.charAt(0).toUpperCase() + text.slice(1);
+    return text.charAt(0).toUpperCase() + text.slice(1);
 };
