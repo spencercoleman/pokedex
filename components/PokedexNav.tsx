749178751a797b03ba2613b55b97a3021f7b48b4
spencercoleman <spencer@edc.vc>
2023-03-18 22:43:31 -0700

    Add chevron icons to PokemonNav

--- components/PokedexNav.tsx
+++ components/PokedexNav.tsx
@@ -1,46 +1,57 @@
 import { formatPokemonID, formatText, type Pokemon } from '../utils';
-import { Box, Flex, LinkBox, SimpleGrid, Text } from '@chakra-ui/react';
+import { Box, Card, Flex, LinkBox, SimpleGrid, Text } from '@chakra-ui/react';
+import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
 import NextLink from 'next/link';
 
 interface PokedexNavProps {
-	prevPokemon: Pokemon | null;
-	nextPokemon: Pokemon | null;
+    prevPokemon: Pokemon | null;
+    nextPokemon: Pokemon | null;
 }
 
 export default function PokedexNav({
-	prevPokemon,
-	nextPokemon,
+    prevPokemon,
+    nextPokemon,
 }: PokedexNavProps) {
-	return (
-		<SimpleGrid columns={2} py={2}>
-			{prevPokemon ? (
-				<LinkBox as={NextLink} href={`/${prevPokemon.name}`}>
-					<Flex gap={1}>
-						<Text color="GrayText" fontSize="sm">
-							#{formatPokemonID(prevPokemon.id)}
-						</Text>
-						<Text fontSize="sm" as="b">
-							{formatText(prevPokemon.name)}
-						</Text>
-					</Flex>
-				</LinkBox>
-			) : (
-				<Box />
-			)}
-			{nextPokemon ? (
-				<LinkBox as={NextLink} href={`/${nextPokemon.name}`}>
-					<Flex flexDirection="row-reverse" gap={1}>
-						<Text color="GrayText" fontSize="sm">
-							#{formatPokemonID(nextPokemon.id)}
-						</Text>
-						<Text fontSize="sm" as="b">
-							{formatText(nextPokemon.name)}
-						</Text>
-					</Flex>
-				</LinkBox>
-			) : (
-				<Box />
-			)}
-		</SimpleGrid>
-	);
+    return (
+        <SimpleGrid columns={2} gap={3} my={3}>
+            {prevPokemon ? (
+                <LinkBox as={NextLink} href={`/${prevPokemon.name}`}>
+                    <Card variant="filled" py={3} px={1}>
+                        <Flex gap={1} alignItems="center">
+                            <ChevronLeftIcon boxSize={6} />
+                            <Text color="GrayText" fontSize="sm">
+                                #{formatPokemonID(prevPokemon.id)}
+                            </Text>
+                            <Text fontSize="sm" as="b">
+                                {formatText(prevPokemon.name)}
+                            </Text>
+                        </Flex>
+                    </Card>
+                </LinkBox>
+            ) : (
+                <Box />
+            )}
+            {nextPokemon ? (
+                <LinkBox as={NextLink} href={`/${nextPokemon.name}`}>
+                    <Card variant="filled" py={3} px={1}>
+                        <Flex
+                            flexDirection="row-reverse"
+                            gap={1}
+                            alignItems="center"
+                        >
+                            <ChevronRightIcon boxSize={6} />
+                            <Text color="GrayText" fontSize="sm">
+                                #{formatPokemonID(nextPokemon.id)}
+                            </Text>
+                            <Text fontSize="sm" as="b">
+                                {formatText(nextPokemon.name)}
+                            </Text>
+                        </Flex>
+                    </Card>
+                </LinkBox>
+            ) : (
+                <Box />
+            )}
+        </SimpleGrid>
+    );
 }
