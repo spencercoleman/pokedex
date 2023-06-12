32019b59bb6c2a697bf4498becef4b5bfbf3388c
spencercoleman <spencer@edc.vc>
2023-06-12 12:32:33 -0700

    Resolve incorrect language bug with Pokemon flavor text in getPokemon

--- utils/pokemon.ts
+++ utils/pokemon.ts
@@ -1,8 +1,8 @@
 import {
-	MainClient,
-	PokemonAbility,
-	PokemonSpecies,
-	PokemonStat,
+    MainClient,
+    PokemonAbility,
+    PokemonSpecies,
+    PokemonStat,
 } from 'pokenode-ts';
 
 const POKEMON_MAX = 905; // Highest ID to fetch
@@ -11,82 +11,83 @@ const DEFAULT_LIMIT = 20; // Limit results to 20 per page
 const client = new MainClient();
 
 export interface PokemonPreview {
-	id: number;
-	name: string;
-	sprite: string | null;
-	types: string[];
+    id: number;
+    name: string;
+    sprite: string | null;
+    types: string[];
 }
 
 export interface Pokemon extends PokemonPreview {
-	abilities: PokemonAbility[];
-	flavorText: string;
-	height: number;
-	stats: PokemonStat[];
-	weight: number;
+    abilities: PokemonAbility[];
+    flavorText: string;
+    height: number;
+    stats: PokemonStat[];
+    weight: number;
 }
 
 export async function getPokemonPreviews(
-	offset: number = 1,
-	limit: number = DEFAULT_LIMIT
+    offset: number = 1,
+    limit: number = DEFAULT_LIMIT
 ): Promise<PokemonPreview[]> {
-	const pokemonSpeciesPromises: Promise<PokemonSpecies>[] = [];
+    const pokemonSpeciesPromises: Promise<PokemonSpecies>[] = [];
 
-	for (let i = offset; i < Math.min(offset + limit, POKEMON_MAX); i++) {
-		pokemonSpeciesPromises.push(client.pokemon.getPokemonSpeciesById(i));
-	}
+    for (let i = offset; i < Math.min(offset + limit, POKEMON_MAX); i++) {
+        pokemonSpeciesPromises.push(client.pokemon.getPokemonSpeciesById(i));
+    }
 
-	const pokemonSpeciesData = await Promise.all(pokemonSpeciesPromises);
+    const pokemonSpeciesData = await Promise.all(pokemonSpeciesPromises);
 
-	const pokemonDataPromises = pokemonSpeciesData.map(
-		async (pokemonSpecies) => {
-			const pokemon = await client.pokemon.getPokemonById(
-				pokemonSpecies.id
-			);
+    const pokemonDataPromises = pokemonSpeciesData.map(
+        async (pokemonSpecies) => {
+            const pokemon = await client.pokemon.getPokemonById(
+                pokemonSpecies.id
+            );
 
-			return {
-				id: pokemonSpecies.id,
-				name: pokemonSpecies.name,
-				sprite: pokemon.sprites.front_default,
-				types: pokemon.types.map((typeData) => typeData.type.name),
-			};
-		}
-	);
+            return {
+                id: pokemonSpecies.id,
+                name: pokemonSpecies.name,
+                sprite: pokemon.sprites.front_default,
+                types: pokemon.types.map((typeData) => typeData.type.name),
+            };
+        }
+    );
 
-	const pokemonPreviews = await Promise.all(pokemonDataPromises);
+    const pokemonPreviews = await Promise.all(pokemonDataPromises);
 
-	return pokemonPreviews;
+    return pokemonPreviews;
 }
 
 export async function getPokemon(
-	identifier: string | number
+    identifier: string | number
 ): Promise<Pokemon> {
-	let pokemonSpeciesData;
+    let pokemonSpeciesData;
 
-	if (typeof identifier === 'string') {
-		pokemonSpeciesData = await client.pokemon.getPokemonSpeciesByName(
-			identifier
-		);
-	} else {
-		pokemonSpeciesData = await client.pokemon.getPokemonSpeciesById(
-			identifier
-		);
-	}
+    if (typeof identifier === 'string') {
+        pokemonSpeciesData = await client.pokemon.getPokemonSpeciesByName(
+            identifier
+        );
+    } else {
+        pokemonSpeciesData = await client.pokemon.getPokemonSpeciesById(
+            identifier
+        );
+    }
 
-	const pokemonData = await client.pokemon.getPokemonById(
-		pokemonSpeciesData.id
-	);
+    const pokemonData = await client.pokemon.getPokemonById(
+        pokemonSpeciesData.id
+    );
 
-	return {
-		abilities: pokemonData.abilities,
-		flavorText: pokemonSpeciesData.flavor_text_entries[6].flavor_text
-			.split('')
-			.join(' '),
-		height: pokemonData.height,
-		id: pokemonSpeciesData.id,
-		name: pokemonSpeciesData.name,
-		sprite: pokemonData.sprites.front_default,
-		stats: pokemonData.stats,
-		types: pokemonData.types.map((typeData) => typeData.type.name),
-		weight: pokemonData.weight,
-	};
+    return {
+        abilities: pokemonData.abilities,
+        flavorText: pokemonSpeciesData.flavor_text_entries
+            .filter((entry) => entry.language.name === 'en')[0]
+            .flavor_text.split('')
+            .join(' '),
+        height: pokemonData.height,
+        id: pokemonSpeciesData.id,
+        name: pokemonSpeciesData.name,
+        sprite: pokemonData.sprites.front_default,
+        stats: pokemonData.stats,
+        types: pokemonData.types.map((typeData) => typeData.type.name),
+        weight: pokemonData.weight,
+    };
 }
