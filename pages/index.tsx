0527714dfc3725ee37618da9ca48e5a8c908689b
spencercoleman <spencer@edc.vc>
2023-06-14 18:12:55 -0700

    Remove load button and implement infinite scroll to load more pokemon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { GetStaticProps, NextPage } from 'next';
 import { getPokemonPreviews, type PokemonPreview } from '../utils';
-import { Heading } from '@chakra-ui/react';
+import { Flex, Heading, Spinner } from '@chakra-ui/react';
 import PokemonSearch from '../components/PokemonSearch';
 import PokemonList from '../components/PokemonList';
 
@@ -10,15 +10,37 @@ interface HomeProps {
 }
 
 const Home: NextPage<HomeProps> = ({ initialPokemon }) => {
-    const OFFSET_INCREMENT = 20;
+    const OFFSET_INCREMENT = 48;
     const [offset, setOffset] = useState(OFFSET_INCREMENT + 1); // Add 1 to handle the initial pokemon starting at ID 1
     const [pokemon, setPokemon] = useState(initialPokemon);
+    const [isLoading, setIsLoading] = useState(false);
 
     const fetchAdditionalPokemon = async () => {
+        setIsLoading(true);
+
         const newPokemon = await getPokemonPreviews(offset, OFFSET_INCREMENT);
+
         setPokemon([...pokemon, ...newPokemon]);
         setOffset(offset + OFFSET_INCREMENT);
+        setIsLoading(false);
     };
+
+    const handleScroll = () => {
+        if (
+            window.innerHeight + document.documentElement.scrollTop !==
+                document.documentElement.offsetHeight ||
+            isLoading
+        ) {
+            return;
+        }
+
+        fetchAdditionalPokemon();
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [isLoading]);
 
     return (
         <div>
@@ -28,14 +50,19 @@ const Home: NextPage<HomeProps> = ({ initialPokemon }) => {
             <main>
                 <PokemonSearch />
                 <PokemonList pokemon={pokemon} />
-                <button onClick={fetchAdditionalPokemon}>Load More</button>
+
+                {isLoading && (
+                    <Flex justifyContent="center" pt={3}>
+                        <Spinner color="red.500" />
+                    </Flex>
+                )}
             </main>
         </div>
     );
 };
 
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
-    const initialPokemon = await getPokemonPreviews(); // Get the first 1-20 pokemon
+    const initialPokemon = await getPokemonPreviews(1, 48); // Get the first 1-20 pokemon
 
     return {
         props: {
